74e47aede565aa49c834adff1bd55b2c247e4e6d
Ali Hasan Md. Linkon <39150694+Linkon45@users.noreply.github.com>
2021-02-27 02:26:32 +0600

    Login Signup added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import BusinessScreen from './screens/BusinessScreen'
 import BusinessDetailsScreen from './screens/BusinessDetailsScreen'
+import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 
@@ -18,6 +20,8 @@ const App = () => {
           <Route path='/' component={HomeScreen} exact />
           <Route path='/business/:id' component={BusinessScreen} />
           <Route path='/businessdetails/:id' component={BusinessDetailsScreen} />
+          <Route path='/login' component={LoginScreen} />
+          <Route path='/register' component={RegisterScreen} />
         </Container>
       </main>
       <Footer />
---
+++ src/screens/RegisterScreen.js
@@ -0,0 +1,157 @@
+import React from 'react'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { Form, Button, Row, Col } from 'react-bootstrap'
+import FormContainer from '../components/FormContainer'
+
+const RegisterScreen = () => {
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+    const [contactNumber, setContactNumber] = useState('')
+    const [address, setAddress] = useState('')
+    const [category, setCategory] = useState('')
+    const [products, setProducts] = useState('')
+    const [description, setDescription] = useState('')
+    const [image,setImage]=useState('')
+
+
+
+    const uploadFileHandler = async (e) => {
+      
+      }
+    
+      const submitHandler = (e) => {
+        e.preventDefault()
+        
+      }
+
+    return (
+        <FormContainer>
+            <h1>Sign Up</h1>
+            <Form onSubmit={submitHandler}>
+                <Form.Group controlId='name'>
+                    <Form.Label>Business Name</Form.Label>
+                    <Form.Control
+                        type='name'
+                        placeholder='Enter name'
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    ></Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId='email'>
+                    <Form.Label>Email Address</Form.Label>
+                    <Form.Control
+                        type='email'
+                        placeholder='Enter email'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    ></Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId='contactNumber'>
+                    <Form.Label>Contact Number</Form.Label>
+                    <Form.Control
+                        type='number'
+                        placeholder='Enter Contact Number'
+                        value={contactNumber}
+                        onChange={(e) => setContactNumber(e.target.value)}
+                    ></Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId='address'>
+                    <Form.Label>Address</Form.Label>
+                    <Form.Control
+                        type='text'
+                        placeholder='Enter Address'
+                        value={address}
+                        onChange={(e) => setAddress(e.target.value)}
+                    ></Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId="category">
+                    <Form.Label>Select category</Form.Label>
+                    <Form.Control
+                        as="select"
+                        value={category}
+                        onChange={e => {
+                            console.log("e.target.value", e.target.value);
+                            setCategory(e.target.value);
+                        }}>
+                        <option value="DICTUM">Dictamen</option>
+                        <option value="CONSTANCY">Constancia</option>
+                        <option value="COMPLEMENT">Complemento</option>
+                    </Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId="products">
+                    <Form.Label>Enter product name (Separate product by comma)</Form.Label>
+                    <Form.Control as="textarea" rows={3}
+                        placeholder='Enter Products Name'
+                        value={products}
+                        onChange={(e) => setProducts(e.target.value)}>
+                    </Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId='description'>
+                    <Form.Label>Description</Form.Label>
+                    <Form.Control
+                        as="textarea" rows={3}
+                        placeholder='Enter description'
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                    ></Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId='image'>
+                    <Form.Label>Image</Form.Label>
+                    <Form.Control
+                        type='text'
+                        placeholder='Enter image url'
+                        value={image}
+                        onChange={(e) => setImage(e.target.value)}
+                    ></Form.Control>
+                    <Form.File
+                        id='image-file'
+                        label='Choose File'
+                        custom
+                        onChange={uploadFileHandler}
+                    ></Form.File>
+                </Form.Group>
+
+
+
+                <Form.Group controlId='password'>
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        type='password'
+                        placeholder='Enter password'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    ></Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId='confirmPassword'>
+                    <Form.Label>Confirm Password</Form.Label>
+                    <Form.Control
+                        type='password'
+                        placeholder='Confirm password'
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                    ></Form.Control>
+                </Form.Group>
+
+                <Button type='submit' variant='primary'>
+                    Register
+        </Button>
+            </Form>
+
+            
+        </FormContainer>
+    )
+}
+
+export default RegisterScreen
