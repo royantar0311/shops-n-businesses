9f91c5e4bdcd5fa7205627703d0c5e8ae4c60fb4
Antar Roy <royantar1@gmail.com>
2021-03-07 19:47:45 +0600

    added pagination on searchscreen

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
-import { Row, Col } from 'react-bootstrap'
+import { Row, Col, Pagination } from 'react-bootstrap'
 import Category from '../components/Category'
 import Business from '../components/Business'
 import Loader from '../components/Loader'
 
+const perPageMax = 1;
+
+
 const SearchScreen = ({ match, categories, businesses }) => {
     const [search, setSearch] = useState(undefined)
-    const [filteredCategory, setFilteredCategory] = useState(undefined)
-    const [filteredBusiness, setFilteredBusiness] = useState(undefined)
+    const [filteredCategory, setFilteredCategory] = useState([]);
+    const [filteredBusiness, setFilteredBusiness] = useState([]);
+    const [paginatedBusiness, setPaginatedBusiness] = useState([]);
+    const [paginatedCategory, setPaginatedCategory] = useState([]);
+    const [activeCategory, setActiveCategory] = useState(1);
+    const [activeBusiness, setActiveBusiness] = useState(1);
 
     useEffect(() => {
         setSearch(match.params.id.toLowerCase())
@@ -20,24 +27,64 @@ const SearchScreen = ({ match, categories, businesses }) => {
             || bus.description.toLowerCase().includes(search) === true
             || bus.products.toLowerCase().includes(search) === true)
         setFilteredBusiness(temp2);
-
-
-        // console.log("Hello" + categories)
     }, [categories, businesses, match, search])
 
+    useEffect(() => {
+        let index = activeCategory;
+        index--;
+        let temp = [];
+        for(let i=index*perPageMax;i<index*perPageMax+perPageMax && i<filteredCategory.length;i++){
+            temp.push(filteredCategory[i]);
+        }
+        setPaginatedCategory(temp);
+    }, [activeCategory, filteredCategory])
 
-    // useEffect(() => {
+    useEffect(() => {
+        let index = activeBusiness;
+        index--;
+        let temp = [];
+        for(let i=index*perPageMax;i<index*perPageMax+perPageMax && i<filteredBusiness.length;i++){
+            temp.push(filteredBusiness[i]);
+        }
+        setPaginatedBusiness(temp);
+    }, [setPaginatedBusiness, activeBusiness, filteredBusiness])
 
+    const paginateCategory = (e) => {
+        e.preventDefault();
+        let index = Number(e.target.innerText);
+        if(isNaN(index))return;
+        setActiveCategory(index);
+    }
 
-    // }, [categories])
+    const paginateBusiness = (e) => {
+        e.preventDefault();
+        let index = Number(e.target.innerText);
+        if(isNaN(index))return;
+        setActiveBusiness(index);
+    }
+    let categoryItems = [];
+    for (let number = 1; number <= Math.ceil(filteredCategory.length/perPageMax); number++) {
+        categoryItems.push(
+            <Pagination.Item key={number} onClick={paginateCategory} active={number === activeCategory}>
+            {number}
+            </Pagination.Item>,
+        );
+    }
+
+    let businessItems = [];
+    for (let number = 1; number <= Math.ceil(filteredBusiness.length/perPageMax); number++) {
+        businessItems.push(
+            <Pagination.Item key={number} onClick={paginateBusiness} active={number === activeBusiness}>
+            {number}
+            </Pagination.Item>,
+        );
+    }
     if (filteredCategory === undefined || match.params.id === undefined || filteredBusiness === undefined) return <Loader />;
     return (
         <>
-            {/* {console.log(match.params.id)} */}
             <h1>Categories</h1>
-            {/* {console.log(filteredCategory)} */}
             <Row className="text-right">
-                {filteredCategory.map((category) => {
+                {paginatedCategory.map((category) => {
                     return (
                         <Col key={category.uid} sm={12} md={6} lg={4} xl={3}>
                             <Category key={category.uid} category={category} />
@@ -45,13 +92,16 @@ const SearchScreen = ({ match, categories, businesses }) => {
                     )
                 })}
             </Row>
+            <div>
+                <Pagination size="sm">{categoryItems}</Pagination>
+            </div>
 
 
             <h1 className="my-3">Business</h1>
             {
-                filteredBusiness.length === 0 ? <p>Businesses are coming</p> :
+                paginatedBusiness.length === 0 ? <p>Businesses are coming</p> :
                     <Row>
-                        {filteredBusiness.map((busi) => {
+                        {paginatedBusiness.map((busi) => {
                             if (busi.isApproved === false) return null;
                             return (<Col key={busi.uid} sm={12} md={6} lg={4} xl={3}>
                                 <Business busi={busi} />
@@ -59,6 +109,9 @@ const SearchScreen = ({ match, categories, businesses }) => {
                         })}
                     </Row>
             }
+            <div>
+                <Pagination size="sm">{businessItems}</Pagination>
+            </div>
 
         </>
     )
