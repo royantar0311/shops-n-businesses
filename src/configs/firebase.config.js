4ae16e916fdca26dde8005331738031e816d3fe1
Antar Roy <royantar1@gmail.com>
2021-03-31 14:12:26 +0600

    going public :tada:

--- src/configs/firebase.config.js
+++ src/configs/firebase.config.js
@@ -3,12 +3,7 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCOywp9HE88SZNRi0vWy7NM1-NbvXR5Q64",
-    authDomain: "shops-upwork.firebaseapp.com",
-    projectId: "shops-upwork",
-    storageBucket: "shops-upwork.appspot.com",
-    messagingSenderId: "633007557034",
-    appId: "1:633007557034:web:7d7f3eec792199266e83d3"
+    // paste your config here
 };
 firebase.initializeApp(firebaseConfig);
 
